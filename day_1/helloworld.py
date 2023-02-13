a9f3529c14246df0622c6b5b332f90f133ec3a2c
ZentoBernabeuPerez <zento@elmandilnegro.es>
2023-02-13 22:21:54 +0100

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,37 @@
+import numpy
+n1 = 3
+n2 = 4
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 % n2)
+print(n1 / n2)
+print(n1 ** n2)
+print(n1 // n2)
+print("Zento")
+print("Bernabéu Pérez")
+print("Spain")
+print("I am enjoying 30 days of Python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type("Zento"))
+print(type("Bernabéu Pérez"))
+print(type("Spain"))
+
+3 # this is an integer
+3.2 # this is float
+3j # this is complex number
+"this is a string so it needs no comment"
+True # this is a boolean
+["this","is","a","list"]
+("this","is","a","tuple")
+{1:"this", 2:"is", 3:"a", 4:"dictionary"}
+{"this","is","a","set"}
+
+dist1 = numpy.linalg.norm(2 - 3)
+dist2 = numpy.linalg.norm(10 - 8)
+print(dist1)
+print(dist2)
